ce166b63f31c0954f4a4a7edcbea2576f9bdcb22
Ngoc Quang, Nguyen <95991125+GnauqTheBeast@users.noreply.github.com>
2023-01-07 10:03:12 +0700

    Add files via upload

---
+++ DethiToanrr.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int x[30];
+
+void init(){
+	cin >> n >> k;
+	for(int i = 1; i <= n - k; i++)
+		x[i] = 0;
+	for(int i = n - k + 1; i <= n; i++)
+		x[i] = 1;
+}
+
+bool check(){
+	int dem = 0;
+	for(int i = 1; i <= n; i++){
+		if(x[i] == 1){
+			dem++;
+			if(dem == k)
+				return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+void display(){
+	for(int i = 1; i <= n; i++)	
+		cout << x[i];
+	cout << endl;
+}
+
+void genNext(){
+	int i = n;
+	while(x[i] == 0) i--;
+	while(x[i] == 1) i--;
+	x[i] = 1;
+	x[i + 1] = 0;
+}
+
+int main() {
+	init();
+	display();
+	while(!check()){
+		genNext();
+		display();
+	}
+}
